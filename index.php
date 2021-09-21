d1aab17fe0c61e17bf58da81b7efe433fefbf4f2
Elysio3 <mael.kerviche@hotmail.fr>
2021-09-21 18:32:54 +0200

    Site réalisé par Maël KERVICHE et Kévin BOURSIER

---
+++ index.php
@@ -0,0 +1,353 @@
+<?php
+session_start();
+?>
+
+
+<html lang="en">
+  <head>
+    <title>Site Harmonisation</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  </head>
+
+
+
+  <link rel="icon" href="images/favicon.ico" />
+  <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/flexslider.css">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+
+
+
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700" rel="stylesheet">
+
+  
+
+  
+
+
+  <body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
+<section id="section-navbar">
+
+
+    <nav class="navbar navbar-expand-lg site-navbar navbar-light bg-light scrolled awake" id="pb-navbar">
+
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <a class="navbar-brand" href="index.php" ><img src="images/icon.jpg" height=90 width=90></a>
+        <div class="navbar-collapse justify-content-md-center collapse" style="">
+            <ul class="navbar-nav text-center">
+
+                <li class="nav-item"><a href="#section-navbar">Accueil</a></li>
+                <li class="nav-item"><a href="#section-presentation">Présentation</a></li>
+                <li class="nav-item"><a href="#section-univers">L'univers</a></li>
+                <li class="nav-item"><a href="#section-jeu">Le Jeu</a></li>
+                <li class="nav-item"><a href="#section-champions">Les champions</a></li>
+                
+
+            </ul>
+        </div>
+      </div>
+    </nav>
+
+</section>
+
+
+
+
+
+    <section class="site-hero" style="background-image: url('images/main-background.jpg');opacity: 0.95;" id="section-home" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row intro-text align-items-center justify-content-center">
+          <div class="col-md-10 text-center">
+            <h1 class="site-heading site-animate fadeInUp site-animated">League of legends<strong></strong></h1>
+            <p class="lead site-subheading mb-4 site-animate fadeInUp site-animated">Présentation du Jeu MOBA</p>
+
+            <!--Bouton ref section présentation-->
+            <p><a href="#section-presentation" class="smoothscroll btn btn-primary px-4 py-3">En savoir plus</a></p>
+            
+           
+          </div>
+        </div>
+    </div>
+    </section> <!-- section -->
+
+    <hr>
+
+    <div class="container" id="section-presentation">
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="site-heading site-animate fadeInUp site-animated">League of legends :<br></h1><h3>Qu'est-ce que c'est ?<strong></strong></h3>
+                <br>
+                <div class="row">
+                    <div class="col-lg-10 text-justify">
+                        <p>
+                            League of Legends (aussi appelé LoL) est sans doute le plus célèbre des MOBA (Multiplayer Online Battle Arena), qu’on peut traduire en français par « arène de bataille en ligne multijoueur ». En d’autres termes, il s’agit d’un jeu d’action (et de stratégie) dont les parties se déroulent dans une arène fermée où s’affrontent des équipes composées de plusieurs joueurs connectés.<br><br>
+                            Sorti en 2009, League of Legends est un jeu gratuit sur le modèle de ce qu’on appelle les free-to-play. Comprenez qu’il est possible de jouer à LoL sans débourser un centime, mais que les occasions de dépenser de l’argent pour agrémenter ses parties peuvent être nombreuses en cours de partie.
+                        </p>
+                    </div>
+                    <div class="col-lg-2">
+                    <img src="images/pegi12.png">
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col text-justify">
+                        <p>
+                        La quasi-totalité des modes de jeu opposent deux équipes de cinq joueurs. Certains d’entre eux proposant des parties en un contre un ou deux contre deux font aussi parfois leur apparition ; une façon pour les développeurs du jeu de tester de nouvelles options mais aussi de permettre aux joueurs de varier les plaisirs.
+                        </p>
+                    </div>
+
+                </div>
+                
+            </div>
+        </div>
+
+        <img src="images/secondary-background.jpg">
+
+    </div>
+
+    <hr>
+
+<div class="container" id="section-univers">
+    <div class="row">
+        <div class="col text-center">
+            <h1 class="site-heading site-animate fadeInUp site-animated">L'univers Runneterra<br></h1><h3>UN ensemble complet d'histoires en tout genre<strong></strong></h3>
+            <br>
+            <div class="row">
+                <div class="col text-center">
+                    <p>
+                        League of legends est constitué de plusieurs régions toutes composé d’une histoire bien distincte. Chaque personnage de league of legends est lié a une de c’est region. Une carte interactive avec l'histoire de chaque région et les différents personnages qui y résident est disponible <button class="btn btn-primary"><a href="https://map.leagueoflegends.com/" target="_blank" style="color: black;"><b>ici</b></a></button>
+                    </p>
+
+                    <p id="scrollable">
+                        Qu'est-ce que Runeterra?<br>
+                        Runeterra est un monde magique dans le jeu de League of Legends. Ce monde fictif est où la plupart des traditions derrière le jeu se déroule. Runeterra est un endroit complet qui a son propre ensemble de règles et de juridictions. Par exemple, la planète a plusieurs formes de gouvernement et des niveaux de technologie.<br>
+                        Les propriétés physiques de Runeterra sont une partie importante de sa géographie historique et actuelle. La planète est actuellement la maison des deux continents Shurima et Valoran. Bien sûr, il y a des soupçons qui existent plus continents, mais ils ont encore à trouver ou nom. Runeterra accueille également des plans d'eau, y compris les mers et les océans, où sont situés plusieurs îles.<br>
+                        Runeterra a une lune de satellite naturel qui tourne autour de lui, alors que l'ensemble tourne planète autour d'une étoile de type G. Le soleil et la lune pour ce monde se retrouvent dans un domaine de l'esprit. Cette connexion pour plusieurs passerelles entre Runeterra et le monde des esprits.
+                        <br><br>
+                        Les Guerres runiques<br>
+                        Les Guerres runiques jouent un grand rôle dans l'histoire de Runeterra. Les guerres ont commencé après « Runes monde », ou des objets anciens ont été découverts. Au fil du temps, les esprits les plus brillants ont étudié les runes pour déterminer ce qu'ils ont été utilisés et quel genre de pouvoir les avait runes. Il y avait différents sur la question, dont certains dit que le monde Runes étaient intrinsèquement mauvais et doit être enfermé.<br>
+                        Une fois que des nouvelles des runes est devenu plus connu, la guerre a éclaté sur la planète. Certains voulaient trouver les runes pour protéger la population, tandis que d'autres recherches pour les libérer les runes à l'intérieur des pouvoirs. Finalement, Ryze a entrepris un voyage en solo pour voir toutes les runes pour protéger la planète de la destruction.
+                        <br><br>
+                        Qu'est-ce qui le rend spécial?<br><br>
+                        Il est intéressant, l'univers a été créé par la magie, qui se reflète dans son nom. Runeterra se traduit par la magie de la Terre. Cette distinction est importante car tous les mondes dans League of Legends répondent à la magie de la même façon.<br>
+                        Il y a des êtres magiques sur toute la planète de Runeterra. Certains sont nés avec elle, tandis que d'autres peuvent apprendre la magie ou l'exécuter à l'aide d'objets magiques et techniques. Les différents niveaux de magie peuvent être largement répartis en magie primaire et combiné.<br>
+                        En outre, Runeterra abrite de nombreuses races différentes. Alors que la plupart sont des êtres humains, il y a d'autres races d'habitants aussi bien. Par exemple, les races primaires sapien comprennent les humains, Ascended, brackern, Célestes, dragons, golems, esprits, observateurs, yétis et trolls. Ce ne sont que quelques-unes des races primaires de Sapiens. Le nombre réel est vaste et diversifié.<br>
+                        Il est intéressant, il y a aussi d'autres races. Ces races ont des qualités que la course primaire, comme les comportements complexes et pensées, mais ils agissent plus comme rôle d'arrière-plan.<br><br>
+                        Remarques finales<br>
+                        Runeterra est une planète avec une riche histoire de la magie. Au fil du temps, il est devenu l'une des principales planètes de la Ligue des jeu Legends. Comprendre son histoire peut vous aider à faire un meilleur joueur dans ce jeu interactif.
+                    </p>
+
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+    <hr>
+
+<div class="container text-center" id="section-jeu">
+    <div class="row">
+        <div class="col text-center">
+            <h1 class="site-heading site-animate fadeInUp site-animated">Comment y jouer ?<br></h1><h3>Un jeu "easy to get, hard to master"<strong></strong></h3>
+            <br>
+            <div class="row">
+                <div class="col text-justify">
+                    <p>
+                    Lors de chaque partie, le joueur dirige son personnage et doit enchaîner coups et sortilèges afin de prendre l’ascendant sur l’équipe adverse. En fonction de son rôle et de ses pouvoirs, il doit aider au mieux sa propre équipe car la coopération est essentielle pour espérer l’emporter. Il est donc aussi important de pouvoir communiquer (souvent par l’intermédiaire d’un micro-casque) avec ses compagnons d’aventure.<br><br>
+                    LoL est très présent dans le monde des compétitions esportive. Il est possible de suivre, par le biais de vidéos diffusées en ligne, de très nombreuses parties. Ces championnats rencontrent en effet un vif succès auprès des adeptes du jeu.<br><br>
+                    Depuis 2019, League of Legends propose un nouveau mode (ou plutôt un jeu dérivé) baptisé Teamfight Tactics (TFT). Sur le principe des jeux d’échecs et de stratégie, ce titre, qui est aussi un free-to-play, invite le joueur à placer ses troupes pour engager des combats automatisés (on parle depuis peu de ce nouveau genre de jeu, nommé auto-chess). Ici, on ne joue pas en équipe mais contre d’autres joueurs – ou contre l’intelligence artificielle lors de certains combats – au cours de duels tactiques. L’action se déroule dans l’univers de LoL (seul véritable point commun avec le jeu original).
+                    </p>
+                    
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <button class="btn btn-primary"><a href="https://www.leagueoflegends.com/fr-fr/how-to-play/" target="_blank" style="color:black;">Découvrez la présentation du jeu sur le site officiel</a></button>
+</div>
+
+<hr>
+
+
+<div class="col text-center">
+
+<button class="btn btn-primary"><a href="index.php?id=uZmUT40Q0OY" style="color:black;">Bande annonce du jeu</a></button>
+
+<button class="btn btn-primary"><a href="index.php?id=l4bf7pjcYYw" style="color:black;">Présentation des règles</a></button>
+</div>
+<br>
+
+<section id="section-video">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+            <!-- ------------- API YOUTUBE GET NOM VIDEO -------------- -->
+            <?php
+                $api_key = "AIzaSyCMpz3Jc1OnJos5kZdfuMEX7Bbk_yhK3Vc";
+                $video_id = "l4bf7pjcYYw";
+                if(isset($_GET['id']))
+                {
+                    $video_id = $_GET['id'];
+                }
+                
+                /*
+                    l4bf7pjcYYw //lol worlds
+                    uZmUT40Q0OY // lol trailer
+                */
+                $url = "https://www.googleapis.com/youtube/v3/videos?id=" . $video_id . "&key=" . $api_key . "&part=snippet,contentDetails,statistics,status";
+                $json = file_get_contents($url);
+                $getData = json_decode( $json , true);
+                foreach((array)$getData['items'] as $key => $gDat){
+                    $title = $gDat['snippet']['title'];
+                }
+                // Output title
+                echo "<h4>Les règles en vidéo : <br>".$title."</h4><br>";
+
+                //<!-- ------------ IFRAME EMBED YOUTUBE VIDEO -------------- -->
+                echo('<iframe id="video-viewer"src="https://www.youtube.com/embed/'. $video_id.'?rel=0&iv_load_policy=3" width="1120" height="630" frameborder="0"></iframe>');
+
+            ?>
+
+            
+            
+            <hr>
+            </div>
+        </div>
+    </div>
+</section>
+        
+    <hr>
+
+    <div class="container" id="section-champions">
+    <div class="row">
+        <div class="col text-center">
+            <h1 class="site-heading site-animate fadeInUp site-animated">Une multitude de champions<br></h1><h3>Accessible pour tout type de joueur<strong></strong></h3>
+            <br>
+            <div class="row">
+                <div class="col text-justify">
+                    <p>
+                    Il existe différent champions que le jouer peut choisir en fonction de son style de jeu, de ses envies et de ses besoins.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+
+
+
+    
+
+
+
+
+<div class="row">
+    <div class="col-lg-2 text-center">
+        <?php
+            $dirs = array_filter(glob('champions/*.html'));
+            foreach($dirs as $value)
+            {
+                $ext = pathinfo($value, PATHINFO_EXTENSION);
+                $nom = basename($value,".".$ext);
+                echo('<button class="btn btn-primary" onclick="toggle(\''.$value.'\')">'.$nom.'</button><br>');
+                echo('<br>');
+            }
+        ?>  
+    </div>
+    <div class="col-lg-8">
+        <?php
+            $dirs = array_filter(glob('champions/*.html'));
+            foreach($dirs as $value)
+            {
+                echo('<div id="'.$value.'" style="display: none;">');
+                include($value);
+                echo('</div>');
+                //echo($value);
+            }
+        ?>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script>
+    function toggle(id)
+    {
+        var div1 = document.getElementById(id);
+        
+
+        if(div1.style.display == 'block')
+        {
+            div1.style.display = 'none';
+        }
+        else
+        {
+            div1.style.display = 'block';
+        }
+    }
+</script>
+
+    <hr>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row mb-3">
+          
+        </div>
+        <div class="row">
+            <p class="col-12 text-center">
+            Copyright © <script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+            </p>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+    
+
+
+
+  </body>
+</html>
